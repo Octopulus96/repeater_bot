cc4b86a111c150b212d627b38c0bf910e826e9df
Makarov Valentine <octopulus96@gmail.com>
2022-05-20 16:24:46 +0300

    Database CRUD operations module

---
+++ service/database/db_crud.py
@@ -0,0 +1,23 @@
+from db_connect import db_session
+from db_models import Dictionary
+
+class DatabaseInteraction():
+
+    def insert_data(self, word: str, description: str) -> Dictionary:
+        data = Dictionary(word=word, description=description)
+        db_session.add(data)
+        db_session.commit()
+
+    def select_data(self, word: str) -> Dictionary:
+        for item in db_session.query(Dictionary).filter(Dictionary.word == word):
+            return item
+
+    def update_data(self, word: str, new_word: str, new_description: str) -> Dictionary:
+        for item in db_session.query(Dictionary).filter(Dictionary.word == word):
+            item.word, item.description = new_word, new_description
+            db_session.commit()
+
+    def delete_data(self, word: str) -> Dictionary:
+        data = db_session.query(Dictionary).filter(Dictionary.word == word)
+        data.delete()
+        db_session.commit()
