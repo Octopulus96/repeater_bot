459fc3dd4bafe8224a797aca81c8571b9168a385
Makarov Valentine <octopulus96@gmail.com>
2022-08-22 16:42:52 +0300

    complited fastapi handlers

--- service/app/main.py
+++ service/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 
+from service.database.db_crud import DatabaseInteraction
+
 app = FastAPI()
+repo = DatabaseInteraction()
+
+@app.post("/post/{word}/{description}")
+def insert_word(word: str, description: str):
+    repo.insert_data(word, description)
 
 
-@app.get("/")
-def get_translation():
-    return {"Hello": "John"}
+@app.get("/get/{word}")
+def select_word(word: str):
+    return repo.select_data(word)
 
-if __name__ == "__main__":
-    uvicorn.run(app)
+
+@app.put("/put/{word}/{new_word}/{new_description}")
+def update_word(word: str, new_word: str, new_description: str):
+    repo.update_data(word, new_word, new_description)
+
+
+@app.delete("/delete/{word}")
+def delete_word(word: str):
+    repo.delete_data(word)
